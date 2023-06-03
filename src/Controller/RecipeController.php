171e12c8e6ff71002f92a81df9a6862ec7e18984
alex dess <alexandre.dessoly1@gmail.com>
2023-06-03 17:50:19 +0200

    edit recipe ok

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -65,4 +65,28 @@ class RecipeController extends AbstractController
         ]);
         
     }
+
+    #[Route('/recette/edition/{id}', name:'app_recipe_edit',methods:['GET','POST'])]
+    public function edit(RecipeRepository $recipeRepository,int $id,Request $request,EntityManagerInterface $manager):Response
+    {
+        $recipe = $recipeRepository->findOneBy(["id" => $id]) ;
+        $form = $this->createForm(RecipeType::class, $recipe);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $recipe = $form->getData();
+            
+            $manager->persist($recipe);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre recette a été modifiée avec succès !'
+            );
+            return $this->redirectToRoute('app_recipe');
+        }
+            return $this->render('pages/recipe/new.html.twig',[
+                'form'=>$form->createView()
+        ]);
+    }
 }
