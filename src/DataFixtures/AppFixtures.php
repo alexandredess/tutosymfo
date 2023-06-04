5a4ced3be13fa56d5f9b17b85d27992694a6ec99
alex dess <alexandre.dessoly1@gmail.com>
2023-06-04 21:21:42 +0200

    hash password entitylistener ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,12 +4,13 @@ namespace App\DataFixtures;
 
 
 use Faker\Factory;
+use App\Entity\User;
 use Faker\Generator;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -55,6 +56,18 @@ class AppFixtures extends Fixture
                     }
             $manager->persist($recipe);
 
+        }
+
+        //Users
+        for ($i=0; $i <10 ; $i++) { 
+            $user = new User();
+            $user -> setName($this->faker->name())
+                ->setPseudo(mt_rand(0,1)===1 ? $this->faker->firstName():null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword("password");            
+
+            $manager->persist($user);
         }
 
         $manager->flush();
